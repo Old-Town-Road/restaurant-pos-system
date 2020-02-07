00974a86d86d7bde9c4888dfc2e25b876ab8b29d
Sultan Al Bogami <saalboga@uncg.edu>
2020-02-06 20:27:54 -0500

    Store and table objects are added

---
+++ src/Models/Store.java
@@ -0,0 +1,38 @@
+package Models;
+
+/**
+ * This class is to have stores information.
+ *
+ * @author Sultan Albogami Last Updated: 2/6/2020
+ */
+
+public class Store {
+
+    int storeId;
+    String storeName;
+
+    public Store(int _storeId, String _storeName) {
+        this.storeId = _storeId;
+        this.storeName = _storeName;
+    }
+
+    // ================= GETTERS ==========================
+
+    public int getStoreId() {
+        return this.storeId;
+    }
+
+    public String getStoreName() {
+        return this.storeName;
+    }
+
+    // ================= SETTERS ==========================
+
+    public void setStoreId(int _storeId) {
+        this.storeId = _storeId;
+    }
+
+    public void setStoreName(String _storeName) {
+        this.storeName = _storeName;
+    }
+}
---
+++ src/Models/Table.java
@@ -0,0 +1,96 @@
+package Models;
+
+/**
+ * This class is construct table model and allow some table functionalities.
+ *
+ * @author Sultan Albogami Last Updated: 2/6/2020
+ */
+
+public class Table {
+
+    private int id;
+    private int userId;
+    private int ticketId;
+    private int checkId;
+    private String name;
+
+    public Table(int _id, int _userId, int _ticketId, int _checkId, String _name) {
+        this.id = _id;
+        this.userId = _userId;
+        this.ticketId = _ticketId;
+        this.checkId = _checkId;
+        this.name = _name;
+    }
+
+    /**
+     * Called when a new check is opened.
+     *
+     * @return
+     */
+    boolean startCheck(int _userId) {
+        return true;
+    }
+
+    /**
+     * Called when a ticket is added to the check.
+     *
+     * @return
+     */
+    boolean addTicket(int _ticketId) {
+        return true;
+    }
+
+    /**
+     * Called when a ticket is cashed out.
+     *
+     * @return
+     */
+    boolean cashOutTicket(int _checkId) {
+        return true;
+    }
+
+    // ================= GETTERS ==========================
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public int getTicketId() {
+        return this.ticketId;
+    }
+
+    public int getCheckId() {
+        return this.checkId;
+    }
+
+    public String getName() {
+        // Return the name
+        return this.name;
+    }
+
+    // ================= SETTERS ==========================
+
+    public void setId(int _id) {
+        this.id = _id;
+    }
+
+    public void setUserId(int _userId) {
+        this.userId = _userId;
+    }
+
+    public void setName(String _name) {
+        this.name = _name;
+    }
+
+    public void setTicketId(_ticketId) {
+        this.ticketId = _ticketId;
+    }
+
+    public void setCheckId(_checkId) {
+        this.checkId = _checkId;
+    }
+}
