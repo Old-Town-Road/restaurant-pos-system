304ba322a18b77ea59d00de16b17890ed23de7ad
Sultan Al Bogami <saalboga@uncg.edu>
2020-02-08 06:14:10 -0500

    Added Main.java and updated DrinkItem.java and Ticket.java

--- src/App/Models/DrinkItem.java
+++ src/App/Models/DrinkItem.java
@@ -2,4 +2,7 @@ package Models;
 
 public class DrinkItem extends MenuItem{
 
+    public DrinkItem(int ID, String menuName, String item, String itemName, int price, int priorityScore, int executionTime) {
+        super(ID, menuName, item, itemName, price, priorityScore, executionTime);
+    }
 }
---
+++ src/App/Models/Main.java
@@ -0,0 +1,18 @@
+package Models;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        testTicket();
+
+    }
+	//Method to test Ticket object
+    public static void testTicket() {
+        Ticket t1 = new Ticket(1, new java.util.Date(System.currentTimeMillis()), 1, 1, null);
+        Ticket t2 = new Ticket(2, new java.util.Date(System.currentTimeMillis()), 2, 2, null);
+        System.out.println(t1);
+        System.out.println(t2);
+    }
+}
+
