09ad6487a2b810851f82934fa0831759d562e11a
Raiana Zaman <raiana.salmin@gmail.com>
2020-04-20 18:18:06 -0400

    Revert "Merge pull request #26 from Old-Town-Road/revert-25-raiana-new"
    This reverts commit 63a04ae9f1e693b550b99ad9c7e3ef0ae4e1fe03.

--- controllers/Runner.java
+++ controllers/Runner.java
@@ -5,6 +5,10 @@ package controllers;
  * @author Sultan Al Bogami Last Updated: 2/22/2020
  */
 
+import java.io.IOException;
+
+import API.GoogleAuthWrapper;
+import API.OAuthException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import views.Frame;
@@ -12,25 +16,25 @@ import views.SetView;
 
 public class Runner extends Application {
 
-    /**
-     * Called when the software starts.
-     *
-     * @param primaryStage
-     * @throws Exception
-     */
-    public void start(Stage primaryStage) throws Exception {
+	/**
+	 * Called when the software starts.
+	 *
+	 * @param primaryStage
+	 * @throws Exception
+	 */
+	public void start(Stage primaryStage) throws Exception {
 
-        // Creating a views.Frame object with log in view as our main view
-        Frame frame = new Frame(primaryStage, SetView.LOG_IN_VIEW);
-    }
+		// Creating a views.Frame object with log in view as our main view
+		Frame frame = new Frame(primaryStage, SetView.LOG_IN_VIEW);
+	}
 
-    /**
-     * Called when the application launches.
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+	/**
+	 * Called when the application launches.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		launch(args);
+	}
 
 }
